b8589cf93e89c868c724dae835d053d07e4db4ed
Patrick <patrick04184@gmail.com>
2024-10-02 18:06:23 +0800

    add sorted time with item

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,6 +1,11 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
 export const usersKey = (userId: string) => `usersKey#${userId}`;
 export const sessionKey = (sessionId: string) => `sessionKey#${sessionId}`;
-export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const usernameUniqueKey = () => `usernames#unique`;
 export const userLikesKey = (userId: string) => `user:likes#${userId}`;
+export const usernamesKey = () => `usernames`;
+
+// items
+export const itemsKey = (itemId: string) => `items#${itemId}`;
+export const itemsByViewsKey = () => `items:views`;
+export const itemsByEndingAtKey = () => `items:endingAt`;
--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -1,5 +1,18 @@
+import { client } from "$services/redis";
+import { itemsByEndingAtKey } from "$services/keys";
+import { deserialize } from "./deserialize";
+
 export const itemsByEndingTime = async (
-	order: 'DESC' | 'ASC' = 'DESC',
-	offset = 0,
-	count = 10
-) => {};
+  order: "DESC" | "ASC" = "DESC",
+  offset = 0,
+  count = 10
+) => {
+  const ids = await client.zRange(itemsByEndingAtKey(), Date.now(), "+inf", {
+    BY: "SCORE",
+    LIMIT: {
+      offset,
+      count,
+    },
+  });
+  console.log("itemsByEndingTime", ids);
+};
