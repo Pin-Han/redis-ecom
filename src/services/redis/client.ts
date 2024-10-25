43d144ab33c95afaabbeed6dd9ec2ffbbf301079
Patrick <patrick04184@gmail.com>
2024-10-25 16:14:16 +0800

    add lua custom script

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,4 +1,4 @@
-import { createClient } from "redis";
+import { createClient, defineScript } from "redis";
 
 const client = createClient({
   socket: {
@@ -6,6 +6,25 @@ const client = createClient({
     port: parseInt(process.env.REDIS_PORT),
   },
   password: process.env.REDIS_PW,
+  scripts: {
+    addOneAndStore: defineScript({
+      NUMBER_OF_KEYS: 1, // number of keys that the script will use
+      SCRIPT: `return redis.call('SET', KEYS[1], 1 + tonumber(ARGV[1]))`, // the script itself
+      transformArguments: (key: string, value: number) => {
+        return [key, value.toString()];
+      },
+      transformReply: (reply: string) => {
+        return reply;
+      },
+    }),
+  },
+});
+
+client.on("connect", async () => {
+  console.log("connected to redis");
+  await client.addOneAndStore("books:count", 1);
+  const result = await client.get("books:count");
+  console.log("this is result", result);
 });
 
 client.on("error", (err) =>
